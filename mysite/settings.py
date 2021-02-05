84ab3bd6524458a1eb3118a3decd41ec51e5327d
Ishan Mitra <ishanmitra020@gmail.com>
2021-02-05 23:30:13 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,4 +128,8 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 django_heroku.settings(locals())
