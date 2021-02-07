c3fa3bd09ef1936948e5a09e98e6634310f78679
Ishan Mitra <ishanmitra020@gmail.com>
2021-02-07 19:40:32 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASEs['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
